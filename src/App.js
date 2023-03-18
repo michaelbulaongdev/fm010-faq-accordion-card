295d381d264959fed283aac02e581ec69ddd4ae6
michaelbulaongdev <michaelbulaongdev@gmail.com>
2023-03-18 10:00:07 +0800

    list mapping

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import {list} from './components/list';
+import {ReactComponent as ArrowSVG} from './images/icon-arrow-down.svg';
+import {
+	Accordion,
+	AccordionSummary,
+	AccordionDetails,
+	Typography,
+} from '@mui/material';
 
 function App() {
 	return (
 		<div className='App'>
-			<h1>Start Here</h1>
+			<div>
+				<h1>FAQ</h1>
+				{list.map((item) => {
+					return (
+						<Accordion>
+							<AccordionSummary>
+								<Typography>{item.question}</Typography>
+								<ArrowSVG />
+							</AccordionSummary>
+							<AccordionDetails>
+								<Typography>{item.answer}</Typography>
+							</AccordionDetails>
+						</Accordion>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
